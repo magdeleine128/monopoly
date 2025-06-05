ae8cdc2f8199fd8879917d8fff875ed41088dbe9
Malek737 <dallelmalek99@gmail.com>
2025-06-05 19:56:46 +0200

    Update Joueur.h

--- Joueur.h
+++ Joueur.h
@@ -1,28 +1,30 @@
 #pragma once
 #include <vector>
-
+#include <string>
 class Joueur {
-public:
-    enum Couleur { ROUGE, BLEU, VERT, JAUNE, AUCUN };
-    Joueur(Couleur couleur);
-    int lancerDe();
-    void payerStand(int montant);
-    void acheterStand(int index, int prix);
-    bool echangerStand(int case1);//cherche s il possede n et n+1 ou n et n-1 aprés trier le vect
-    int getBillets() const;
-    void payerJoueur(int montant);
-    void ajouterArgent(int montant);
-    int getPosition();
-    void avancer(int nbCases);
-    Couleur getCouleur() const;
-    void stand_initialisation(int nbjoueur);
-    void retirStand(int index);
-    int getNbStands(); // Déclaration ajoutée
-    void changerPosition(int case1);
 private:
-    Couleur couleur;
+    std::string couleur;
     int billets;
     int position;
     int nbstands;
     std::vector<int> standPlayer;
+public:
+    Joueur(std::string clr);
+    int lancerDe();
+    void payerStand(int montant);
+    void acheterStand(int index , int prix);
+    bool echangerStand(int case1);//cherche s il possede n et n+1 ou n et n-1 aprés trier le vect
+    int getBillets() const;
+    void payerJoueur(int montant, Joueur joueur1);
+    void ajouterArgent(int montant);
+    int getPosition() ;
+    void avancer(int nbCases);
+    std::string getCouleur();
+    void stand_initialisation(int nbjoueur);
+    void retirStand(int index);
+    int getNbStands();
+    void changerPosition(int case1) ;
+ 
+
+    
 };
