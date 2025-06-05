8d149b337e549e2a25655e850d8e26b90350830d
Malek737 <dallelmalek99@gmail.com>
2025-06-05 19:56:13 +0200

    Update Joueur.cpp

--- Joueur.cpp
+++ Joueur.cpp
@@ -4,16 +4,14 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
-#include<string>
-
+#include <string>
 
 Joueur::Joueur(std::string clr) : couleur(clr), billets(10), position(0), nbstands(10) {
+  
 }
 
 int Joueur::lancerDe() {
-    int de = std::rand() % 6 + 1;
-    avancer(de);
-    return de;
+    return std::rand() % 6 + 1;
 }
 
 void Joueur::payerStand(int montant) {
@@ -39,8 +37,9 @@ int Joueur::getBillets() const {
     return billets;
 }
 
-void Joueur::payerJoueur(int montant) {
+void Joueur::payerJoueur(int montant,Joueur joueur1) {
     billets -= montant;
+    joueur1.ajouterArgent(montant);
 }
 
 void Joueur::ajouterArgent(int montant) {
@@ -55,9 +54,10 @@ void Joueur::avancer(int nbCases) {
     position = (position + nbCases) % 31;
 }
 
-std::string Joueur::getCouleur() const {
+std::string Joueur::getCouleur() {
     return couleur;
 }
+
 void Joueur::retirStand(int case1) {
     auto it = std::find(standPlayer.begin(), standPlayer.end(), case1);
     if (it != standPlayer.end()) {
@@ -69,33 +69,29 @@ void Joueur::retirStand(int case1) {
 bool Joueur::echangerStand(int case1) {
     std::sort(standPlayer.begin(), standPlayer.end());
     auto it = std::find(standPlayer.begin(), standPlayer.end(), case1);
-    if (it == standPlayer.end()) return true; // Si le joueur ne possède pas déjà la case, autoriser
+    if (it == standPlayer.end()) return true;
 
     int index = std::distance(standPlayer.begin(), it);
 
-    // Vérifier les limites
-    if (index + 1 < (int)standPlayer.size() && standPlayer[index + 1] == case1 + 1)
+    if (index + 1 < standPlayer.size() && standPlayer[index + 1] == case1 + 1) {
         return false;
-
-    if (index - 1 >= 0 && standPlayer[index - 1] == case1 - 1)
+    }
+    if (index > 0 && standPlayer[index - 1] == case1 - 1) {
         return false;
+    }
 
     return true;
 }
 
-
 void Joueur::stand_initialisation(int nbjoueur) {
-    if (nbjoueur == 2) {
-        nbstands = 15;
-    }
-    else {
-        nbstands = 10;
-    }
+    nbstands = (nbjoueur == 2) ? 15 : 10;
 }
 
 int Joueur::getNbStands() {
     return nbstands;
 }
+
 void Joueur::changerPosition(int case1) {
     position = case1;
 }
+
