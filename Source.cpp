e7a1f401a8084768835ac0acbcd11ca38b5960ea
Malek737 <dallelmalek99@gmail.com>
2025-06-05 19:55:01 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,56 +1,64 @@
 #include <iostream>
-#include <thread>
-#include <chrono>
 #include "Joueur.h"
 #include "Carte.h"
 #include "Plateau.h"
+#include<vector>
+
 
 int main() {
     std::cout << "Bienvenue dans Monopoly Junior !" << std::endl;
 
     Joueur joueur1("ROUGE");
     Joueur joueur2("BLEU");
-
     Plateau plateau;
     Carte cartesChance;
 
-    joueur1.stand_initialisation(10); // 2 joueurs
-    joueur2.stand_initialisation(10);
-
+    joueur1.stand_initialisation(2); // 2 joueurs
+    joueur2.stand_initialisation(2);
+    plateau.ajouterJoueur(joueur1);
+    plateau.ajouterJoueur(joueur2);
     bool partieTerminee = false;
     int tour = 0;
-
+    // Affichage plateau
+    plateau.afficherPlateau();
     while (!partieTerminee) {
         Joueur& joueurActuel = (tour % 2 == 0) ? joueur1 : joueur2;
-        Joueur& autreJoueur = (tour % 2 == 0) ? joueur2 : joueur1;
 
-        std::cout << "\nTour du joueur " << joueurActuel.getCouleur() << std::endl;
-        std::cout << " (Argent: " << joueurActuel.getBillets() << " euros)" << std::endl;
+        // Affichage info joueur
+        std::cout << "\nTour du joueur " << joueurActuel.getCouleur()
+            << " (Argent: " << joueurActuel.getBillets() << " euros)" << std::endl;
 
-        int anciennePosition = joueurActuel.getPosition();
+        
+
+        // Attente entrée utilisateur
+        std::cout << "Appuyez sur Entree pour lancer le de...";
+        std::cin.ignore();
+        std::cin.get();
+
+        // Lancer de dé
         int de = joueurActuel.lancerDe();
-        std::cout << "Dé lancé: " << de << std::endl;
+        std::cout << "De lance: " << de << std::endl;
 
-        if ((anciennePosition + de) >= 24) {
+        // Passage départ
+        if ((joueurActuel.getPosition() + de) >= 24) {
             joueurActuel.ajouterArgent(2);
-            std::cout << "Vous avez passé par le départ! +2 euros." << std::endl;
+            std::cout << "Vous avez passe par le depart! +2 euros." << std::endl;
         }
 
+        // Déplacement
         joueurActuel.avancer(de);
-        int position = joueurActuel.getPosition();
-        std::cout << "Vous êtes maintenant sur la case " << position << std::endl;
+        std::cout << "Vous etes maintenant sur la case " << joueurActuel.getPosition() << std::endl;
 
-        plateau.actionCase(position, joueurActuel, cartesChance);
+        // Action case
+        plateau.actionCase(joueurActuel.getPosition(), joueurActuel, cartesChance);
 
+        // Vérification faillite
         if (joueurActuel.getBillets() <= 0) {
             std::cout << "Le joueur " << joueurActuel.getCouleur() << " est en faillite !" << std::endl;
             partieTerminee = true;
         }
-        else {
-            std::cout << "Attente de 3 secondes avant le prochain tour..." << std::endl;
-            std::this_thread::sleep_for(std::chrono::seconds(3));
-            tour++;
-        }
+
+        tour++;
     }
 
     std::cout << "Fin de la partie." << std::endl;
