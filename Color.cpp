8e3a2db12c4d450f95edd9f6eaa65a7c4bde30eb
Malek737 <dallelmalek99@gmail.com>
2025-06-16 15:02:24 +0200

    Update Color.cpp

--- Color.cpp
+++ Color.cpp
@@ -1,13 +1,9 @@
 #include "Color.h"
 
 const std::map<std::string, std::string> Colors::colorCodes = {
-    {"red", "\033[31m"},
-    {"green", "\033[32m"},
-    {"yellow", "\033[33m"},
-    {"blue", "\033[34m"},
+   
     {"magenta", "\033[35m"},
     {"cyan", "\033[36m"},
-    {"white", "\033[37m"},
     {"rouge", "\033[31m"},
     {"vert", "\033[32m"},
     {"jaune", "\033[33m"},
